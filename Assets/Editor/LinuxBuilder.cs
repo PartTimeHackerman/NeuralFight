7dc5095f28a5478b49756202e06510064f041b9f
PartTimeHackerman <kuriozalnie@gmail.com>
2018-05-23 17:42:21 +0200

    3

---
+++ Assets/Editor/LinuxBuilder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEngine;
+
+class LinuxBuilder : MonoBehaviour
+{
+
+    [MenuItem("Build/Linux64Headless")]
+    public static void MyBuild()
+    {
+        buildLinux();
+    }
+
+    static void buildLinux()
+    {
+
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = new[] { "Assets/ant.unity"};
+        buildPlayerOptions.locationPathName = "build";
+        buildPlayerOptions.target = BuildTarget.StandaloneLinux64;
+        buildPlayerOptions.options = BuildOptions.EnableHeadlessMode;
+        BuildPipeline.BuildPlayer(buildPlayerOptions);
+    }
+}
