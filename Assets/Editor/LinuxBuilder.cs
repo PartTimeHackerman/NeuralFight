47b07e8456fc7df9673cb5be9c8f98cf2e680be3
PartTimeHackerman <kuriozalnie@gmail.com>
2018-08-13 13:02:50 +0200

    0

--- Assets/Editor/LinuxBuilder.cs
+++ Assets/Editor/LinuxBuilder.cs
@@ -19,7 +19,7 @@ class LinuxBuilder : MonoBehaviour
     {
 
         BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
-        buildPlayerOptions.scenes = new[] { "Assets/animation.unity"};
+        buildPlayerOptions.scenes = new[] { "Assets/anim.unity"};
         buildPlayerOptions.locationPathName = "build/sumo.x86_64";
         buildPlayerOptions.target = BuildTarget.StandaloneLinux;
         buildPlayerOptions.options = BuildOptions.EnableHeadlessMode;
