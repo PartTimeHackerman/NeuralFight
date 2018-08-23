3a7050f198a05f0de667f05d5f400dc0cd633115
PartTimeHackerman <kuriozalnie@gmail.com>
2018-08-23 21:59:45 +0200

    0

---
+++ Assets/Scripts/Utils/PhysicsDrag.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class PhysicsDrag : MonoBehaviour
+{
+    public float drag;
+
+    private Rigidbody Body;
+
+    private void Start()
+    {
+        Body = GetComponent<Rigidbody>();
+
+        Body.drag = 0f;
+    }
+
+    private void FixedUpdate()
+    {
+        Body.velocity *= Mathf.Clamp01(1f - drag * Time.fixedDeltaTime);
+    }
+}
