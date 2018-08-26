ec65e217ff27df2306004c779481ce0137030c68
PartTimeHackerman <kuriozalnie@gmail.com>
2018-08-26 14:13:49 +0200

    0

---
+++ Assets/Scripts/Agent/WalkBW/WalkBWReward.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+class WalkBWReward : MonoBehaviour
+{
+
+    public bool debug = false;
+    private StandingRewardHumanoid standingReward;
+    private VelocityReward velocityFWReward;
+    private VelocityReward velocityUPReward;
+    private BodyParts bodyParts;
+    public float reward;
+    public float standingRewardVal;
+    public float velocityRewardVal;
+
+    #if (UNITY_EDITOR)
+    public DictionaryStringFloat others = new DictionaryStringFloat();
+    #endif
+
+
+    private void Start()
+    {
+        bodyParts = GetComponent<BodyParts>();
+        standingReward = new StandingRewardHumanoid(bodyParts);
+        velocityFWReward = new VelocityReward(Vector2.left, 10f, bodyParts.root);
+        velocityUPReward = new VelocityReward(Vector2.up, 10f, bodyParts.root);
+
+        standingReward.multipler = new[] { 1f, 1f, 0f, 0f, 0f };
+
+        standingReward.Init();
+
+        if (debug)
+            InvokeRepeating("getReward", 0.0f, .1f);
+    }
+
+    public float getReward()
+    {
+        standingRewardVal = standingReward.getReward();
+        velocityRewardVal = velocityFWReward.getReward() - Mathf.Abs(velocityUPReward.getReward());
+        reward = (standingRewardVal + velocityRewardVal * 3f) / 4f;
+        #if (UNITY_EDITOR)
+        others["torsoOverCOMXZReward"] = standingReward.torsoOverCOMXZReward;
+        others["COMOverMeanOfFeetsXZReward"] = standingReward.COMOverMeanOfFeetsXZReward;
+        #endif
+
+        return reward;
+    }
+}
