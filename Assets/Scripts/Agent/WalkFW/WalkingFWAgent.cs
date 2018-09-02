494df880068e31a8cd86a0cafe8633704151fbde
PartTimeHackerman <kuriozalnie@gmail.com>
2018-09-02 15:14:32 +0200

    0

--- Assets/Scripts/Agent/WalkFW/WalkingFWAgent.cs
+++ Assets/Scripts/Agent/WalkFW/WalkingFWAgent.cs
@@ -12,14 +12,12 @@ internal class WalkingFWAgent : Agent
     private Observations observations;
     private Humanoid2DResetPos resetPos;
     private WalkFWReward rewards;
-    private TerminateDuel terminateFn;
     
     public int steps = 0;
     public int maxSteps = 100;
     
     public bool ready = true;
     private float rewardAnim = 0f;
-    public float sumRewards = 0f;
 
     public override void InitializeAgent()
     {
@@ -27,7 +25,6 @@ internal class WalkingFWAgent : Agent
         rewards = GetComponent<WalkFWReward>();
         actions = GetComponent<Humanoid2DActionsAngPos>();
         resetPos = GetComponent<Humanoid2DResetPos>();
-        terminateFn = GetComponent<TerminateDuel>();
         observations.addToRemove(new[]{"root_pos_x"});
 
     }
@@ -44,7 +41,7 @@ internal class WalkingFWAgent : Agent
     {
         agentParameters.numberOfActionsBetweenDecisions =
             Mathf.Max(agentParameters.numberOfActionsBetweenDecisions, 1);
-        if (!agentParameters.onDemandDecision && ready)
+        if (!agentParameters.onDemandDecision)
         {
             RequestAction();
             if (academyStepCounter % agentParameters.numberOfActionsBetweenDecisions == 0)
@@ -63,25 +60,17 @@ internal class WalkingFWAgent : Agent
         List<float> actionsClamped = new List<float>();
         foreach (var var in actions)
             actionsClamped.Add(Mathf.Clamp(var, -1f, 1f));
-
         
         this.actions.applyActions(actionsClamped);
         rewardAnim = rewards.getReward();
-
-        bool terminateAgent = terminateFn.isTerminated();
-
-        //sumRewards += rewardAnim;
+        
         SetReward(rewardAnim);
 
-        if (steps > maxSteps || terminateAgent)
+        if (steps > maxSteps)
         {
-            //sumRewards = sumRewards > 0 ? sumRewards : 0;
             SetReward(rewardAnim);
-            //sumRewards = 0f;
             steps = 0;
-            ready = false;
             resetPos.ResetPosition();
-            ready = true;
             Done();
         }
     }
---
+++ Assets/Scripts/Obstacles/Booster.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+class Booster : MonoBehaviour
+{
+    public bool right = true;
+    public float force = 0f;
+    public float width = 0f;
+    public float friction = 1000f;
+    private Vector3 direction;
+    public BoxCollider ground;
+    private BoxCollider boost;
+    public bool set = false;
+    private PhysicMaterial groundMaterial;
+
+    void Start()
+    {
+        boost = GetComponent<BoxCollider>();
+        setBooster();
+        groundMaterial = new PhysicMaterial("ground");
+        groundMaterial.frictionCombine = PhysicMaterialCombine.Minimum;
+        ground.material = groundMaterial;
+    }
+
+    public void FixedUpdate()
+    {
+        if (set)
+        {
+            setBooster();
+            set = false;
+        }
+    }
+
+    private void setBooster()
+    {
+        direction = right ? Vector3.right : Vector3.left;
+        Vector3 size = ground.size;
+        size.x = width;
+        boost.size = size;
+        ground.size = size;
+        
+        ground.material.staticFriction = friction;
+        ground.material.dynamicFriction = friction;
+
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        other.GetComponent<Rigidbody>().AddForce(direction * force, ForceMode.Force);
+    }
+
+
+}
