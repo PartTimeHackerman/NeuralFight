c8c7e049f995225087e8e306f299000bb7b825cc
PartTimeHackerman <kuriozalnie@gmail.com>
2018-10-16 18:18:35 +0200

    0

--- Assets/Scripts/Agent/Animation/AnimationRewardOLDcs.cs
+++ Assets/Scripts/Agent/Animation/AnimationRewardOLDcs.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
 
-class AnimationReward : MonoBehaviour
+class AnimationRewardOLD : MonoBehaviour
 {
     public bool calcAvgReward = false;
     public bool debug = false;
@@ -19,21 +19,22 @@ class AnimationReward : MonoBehaviour
     private Rigidbody refRoot;
     private Rigidbody root;
 
+    public float rotErr = 0f;
+    public float posErr = 0f;
+    public float reward = 0f;
+
+    private float nextUpdate = .1f;
+
+    public float avgReward = 0f;
+    public float avgRewardSum = 0f;
+    public float avgCounter = 0f;
+
     public float poseRew = 0f;
     public float velocityRew = 0f;
     public float endRew = 0f;
     public float COMRew = 0f;
     public float imitationReward = 0f;
 
-    public float posErrSum;
-    public float velErrSum;
-    public float endPosErrSum;
-    public float comPosErr;
-    
-    public float posErrExp = -2f;
-    public float velErrExp = -.1f;
-    public float endPosErrExp = -40f;
-    public float comPosExp = -10f;
 
     void Start()
     {
@@ -45,6 +46,50 @@ class AnimationReward : MonoBehaviour
         if (debug) InvokeRepeating("getReward", 0.0f, .1f);
     }
 
+    void LateFixedUpdate()
+    {
+        if (isAnimationRef)
+        {
+            Vector3 refRootRot = refRoot.transform.rotation.eulerAngles;
+            Vector3 rootRot = root.transform.rotation.eulerAngles;
+            rootRot.z = -refRootRot.x;
+            root.transform.rotation = Quaternion.Euler(rootRot);
+        }
+
+        if (calcAvgReward)
+        {
+            getReward();
+
+            avgRewardSum += reward;
+            avgCounter++;
+            avgReward = avgRewardSum / avgCounter;
+        }
+    }
+
+    void LateUpdate()
+    {
+        /*if (Time.time >= nextUpdate && debug)
+        {
+            nextUpdate = Time.time + .1f;
+            getReward();
+
+        }
+        if (avgCounter > 10 && debug)
+        {
+            Debug.Log(avgRewardSum);
+            float avgRew = getAvgReward();
+            Debug.Log(avgRew);
+        }
+        */
+    }
+
+    public float getAvgReward()
+    {
+        float avgRew = avgRewardSum / (avgCounter == 0f ? 1 : avgCounter);
+        avgCounter = 0f;
+        avgRewardSum = 0f;
+        return avgRew;
+    }
 
 
     public float getReward()
@@ -54,11 +99,69 @@ class AnimationReward : MonoBehaviour
         endRew = endReward();
         COMRew = COMReward();
         imitationReward = poseRew * .65f + velocityRew * .1f + endRew * .15f + COMRew * .1f;
-        //imitationReward = poseRew * .65f + velocityRew * .1f + endRew * .15f + COMRew * .1f;
-        //imitationReward *= 10f;
-        //imitationReward = Mathf.Clamp(imitationReward, -100f, 100f);
-        imitationReward = RewardFunctions.toleranceInvNoBounds(imitationReward, 1f, .1f, RewardFunction.LONGTAIL);
+        imitationReward *= 10f;
+        imitationReward = Mathf.Clamp(imitationReward, -100f, 100f);
         return imitationReward;
+    }
+
+    private float getPosErr()
+    {
+        float posErrSum = 0f;
+        foreach (KeyValuePair<string, Rigidbody> namedRigid in bodyParts.getNamedRigids())
+        {
+            if (namedRigid.Key.Equals("butt"))
+                continue;
+
+            Vector3 modelRbPos = root.transform.InverseTransformPoint(namedRigid.Value.transform.position);
+            Vector3 refRbPos = refRoot.transform.InverseTransformPoint(referenceBodyParts.getNamedRigids()[namedRigid.Key].transform.position);
+
+            Vector2 v2Model = new Vector2(modelRbPos.z, modelRbPos.y);
+            Vector2 v2Ref = new Vector2(isAnimationRef ? refRbPos.x : refRbPos.z, refRbPos.y);
+
+            float dist = Vector2.Distance(v2Model, v2Ref) - .1f;
+            dist = Mathf.Abs(Mathf.Clamp(dist, 0, 1) - 1f);
+            dist = RewardFunctions.toleranceInvNoBounds(dist, .4f, .1f, RewardFunction.LONGTAIL);
+            posErrSum += dist;
+
+        }
+
+        return posErrSum / bodyPartsCount;
+    }
+
+    private float getRotErr()
+    {
+        float rotErrSum = 0f;
+        foreach (KeyValuePair<string, Rigidbody> namedRigid in bodyParts.getNamedRigids())
+        {
+            if (namedRigid.Key.Equals("butt"))
+                continue;
+
+            float modelRbRot = getRelativeRot(namedRigid.Value, root, false);
+            float refRbRot = getRelativeRot(referenceBodyParts.getNamedRigids()[namedRigid.Key], refRoot, isAnimationRef);
+
+            float rotDiff = 0f;
+
+
+            if ((modelRbRot > 0 && refRbRot < 0) || (modelRbRot < 0 && refRbRot > 0))
+            {
+                rotDiff = Mathf.Abs(modelRbRot) + Mathf.Abs(refRbRot);
+                if (rotDiff > 1)
+                {
+                    rotDiff = 2f - rotDiff;
+                }
+            }
+            else
+            {
+                rotDiff = Mathf.Abs(Mathf.Abs(modelRbRot) - Mathf.Abs(refRbRot));
+            }
+            rotDiff = Mathf.Abs(rotDiff - 1);
+            rotErrSum += rotDiff;
+
+        }
+
+        return rotErrSum;
+
+
     }
 
     private float getRelativeRot(Rigidbody rigidbody, Rigidbody root, bool isRef)
@@ -67,7 +170,10 @@ class AnimationReward : MonoBehaviour
         float rotClamped = 0f;
         Vector3 rot = (Quaternion.Inverse(root.rotation) * rigidbody.transform.rotation).eulerAngles;
 
-        rotAng = isRef ? rot.x : rot.z;
+        if (isRef)
+            rotAng = rot.x;
+        else
+            rotAng = rot.z;
 
         if (rotAng <= 180f)
             rotClamped = rotAng / 180f;
@@ -79,7 +185,7 @@ class AnimationReward : MonoBehaviour
 
     private float poseReward()
     {
-        posErrSum = 0f;
+        float posErrSum = 0f;
         foreach (KeyValuePair<string, Rigidbody> namedRigid in bodyParts.getNamedRigids())
         {
             if (namedRigid.Key.Equals("butt"))
@@ -106,12 +212,12 @@ class AnimationReward : MonoBehaviour
             posErrSum += Mathf.Pow(posDiff, 2f);
 
         }
-        return Mathf.Exp(posErrExp * posErrSum);
+        return Mathf.Exp(-2f * posErrSum);
     }
 
     private float velocityReward()
     {
-        velErrSum = 0f;
+        float velErrSum = 0f;
         foreach (KeyValuePair<string, Rigidbody> namedRigid in bodyParts.getNamedRigids())
         {
             if (namedRigid.Key.Equals("butt"))
@@ -126,12 +232,12 @@ class AnimationReward : MonoBehaviour
 
         }
 
-        return Mathf.Exp(velErrExp * velErrSum);
+        return Mathf.Exp(-.1f * velErrSum);
     }
 
     private float endReward()
     {
-        endPosErrSum = 0f;
+        float endPosErrSum = 0f;
         foreach (KeyValuePair<string, Rigidbody> namedRigid in bodyParts.getNamedRigids())
         {
             if (!namedRigid.Key.Contains("_end"))
@@ -148,7 +254,7 @@ class AnimationReward : MonoBehaviour
 
         }
 
-        return Mathf.Exp(endPosErrExp * endPosErrSum);
+        return Mathf.Exp(-40f * endPosErrSum);
     }
 
     private float COMReward()
@@ -158,8 +264,8 @@ class AnimationReward : MonoBehaviour
         Vector3 refCOM = physics.getCenterOfMass(referenceBodyParts.getRigids()) - referenceBodyParts.root.transform.position;
         COM.z = 0;
         refCOM.z = 0;
-        comPosErr = Vector3.Distance(COM, refCOM);
+        float dist = Vector3.Distance(COM, refCOM);
 
-        return Mathf.Exp(comPosExp * Mathf.Pow(comPosErr, 2f));
+        return Mathf.Exp(-10f * Mathf.Pow(dist, 2f));
     }
 }
